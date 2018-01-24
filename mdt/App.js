991852332f874618b636344c2a1b9fbb8bfaf1de
Guy Tzaban <guy@panpwr.com>
2018-01-24 14:34:15 +0200

    add stackNavigator

--- mdt/App.js
+++ mdt/App.js
@@ -1,21 +1,35 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { StackNavigator } from 'react-navigation';
+
+
+const HomeScreen = () => (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Home Screen</Text>
+    </View>
+);
+
+const DetailsScreen = () => (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Details Screen</Text>
+    </View>
+);
+
+const RootNavigator = StackNavigator({
+    Home: {
+        screen: HomeScreen,
+    },
+    Details: {
+        screen: DetailsScreen,
+    },
+});
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <View>
+        <RootNavigator />
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
